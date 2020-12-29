0ca60e71ac5d13f8e47faafef8fd9263faf669c1
Michael Waldman <michael.waldman@zumper.com>
2020-12-29 12:37:49 -0500

    add component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Joker's Drink Stash
       </header>
+      <StashSelector name="NextChef" />
     </div>
   );
+}
+
+class StashSelector extends Component {
+    render() {
+      return <div class="stash-selector">my name is {this.props.name}</div>;
+    }
 }
 
 export default App;
